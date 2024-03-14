2ab2e60e9808ae4e50386b901ba5be3e47759b25
felipe <felipestylex08@gmail.com>
2024-03-14 15:16:01 -0300

    feat: create UserRepository

---
+++ src/main/java/com/digitalWallet/digitalWallet/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.digitalWallet.digitalWallet.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.digitalWallet.digitalWallet.domain.user.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+  
+  Optional<User> findUserByDocument(String document);
+  Optional<User> findUserById(Long id);
+
+}
